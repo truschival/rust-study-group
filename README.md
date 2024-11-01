9f85e71b57a5d0554c98250dbf1b55dbed03e220
Thomas Ruschival <thomas@ruschival.de>
2024-11-01 14:24:02 +0100

    Chapter 3-4 Ownership
    Signed-off-by: Thomas Ruschival <thomas@ruschival.de>

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# rust-study-group
-My first steps in rust
+# NOTHING TO SEE HERE - GO ON!
+
+You will only find experiments that make no sense to an experienced Rust developer. And newbies definitely should not look at this code - I am still learning.
+
+
---
+++ ownership/src/main.rs
@@ -0,0 +1,101 @@
+/*
+ * Questions:
+ * - Wo 'lebt' die variable Vec<&str> wenn ich sie nur referenziere?
+ * - gibt es den typ str (String slice) oder ist das immer eine referenz?
+ */
+
+fn split_string(input: &str) -> Vec<&str> {
+    let mut snippets: Vec<&str> = Vec::new();
+
+    let mut start: usize = 0;
+    let mut end: usize = 0;
+
+    while start < input.len() && end < input.len() {
+        // Interesting, man kann eine string-slice nicht direkt als array indizieren...
+        // input[start] funktioniert nicht
+        if input.as_bytes()[start] == b' ' {
+            start += 1;
+            end = start;
+            continue;
+        }
+
+        if input.as_bytes()[end] != b' ' {
+            end += 1;
+            continue;
+        }
+
+        snippets.push(&input[start..end]);
+        end += 1;
+        start = end;
+    }
+    snippets
+}
+//------------------------------------------------------------------------------
+
+fn print_snippets(teststring: &str, snippets: &Vec<&str>) {
+    println!("{teststring} has {} snippets", snippets.len());
+    for s in snippets {
+        println!("|{s}|");
+    }
+}
+//------------------------------------------------------------------------------
+
+fn test_splitstring() {
+    let empty_str = "";
+    let only_spaces = "    ";
+    let starts_start_with_spaces = "       hello world!";
+    let ends_with_spaces = "hello world!   ";
+
+    /*
+    Wo ist die variable Vec<&str> von der ich mir die referenz hole?
+    */
+    print_snippets(empty_str, &split_string(empty_str));
+    print_snippets(only_spaces, &split_string(only_spaces));
+    print_snippets(
+        starts_start_with_spaces,
+        &split_string(starts_start_with_spaces),
+    );
+    print_snippets(ends_with_spaces, &split_string(ends_with_spaces));
+}
+//------------------------------------------------------------------------------
+
+fn pass_through<T>(name: &str, var: T) -> T {
+    println!("pass through variable {name}");
+    var
+}
+
+//------------------------------------------------------------------------------
+fn sink_var<T>(name: &str, _var: T) {
+    println!("variable {name} can't be used anymore");
+}
+
+//------------------------------------------------------------------------------
+fn main() {
+    let primitive_var = 42;
+    let heap_var: String = String::from("Variable on Heap");
+
+    // Fun fact: rust does not like variables named 'foo'
+    let foo = pass_through("heap_var", heap_var);
+    let bar = pass_through("primitive_var", primitive_var);
+
+    // Some references
+    let rfoo = &foo;
+    let r2foo = &foo;
+    println!("Print refs: {r2foo}, {rfoo}");
+
+    // 1 Takes ownership
+    sink_var("foo", foo);
+    //    println!("heap_var is gone! {heap_var}");
+    //println!("Print refs: {r2foo}, {rfoo}");
+
+    sink_var("bar", bar);
+    println!("of course primitive_var can still be used");
+
+    // gibt es den typ str (String slice) oder ist das immer eine referenz?
+    // let aString = String::from("Some literal");
+    // let aSlice: str = aString[2..];
+    // println!("aSlice: {aSlice}");
+
+    println!("---------- Snippets ---------");
+    test_splitstring();
+}
