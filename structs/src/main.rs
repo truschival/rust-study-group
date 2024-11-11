b4df36f30ae3c01a456e3a81aa47c58ee098d8eb
Thomas Ruschival <thomas.ruschival@festo.com>
2024-11-11 15:25:01 +0100

    Structs: add getter & setter
    Signed-off-by: Thomas Ruschival <thomas.ruschival@festo.com>

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,6 +1,6 @@
 use std::f32::consts::PI;
 
-#[derive(Debug)]
+#[derive(Debug)] // derived trait, bin gespannt auf das kapitel
 struct User {
     is_active: bool,
     name: String,
@@ -10,7 +10,7 @@ struct User {
     // how can I create linked lists
     // prev: &User, // refs cannot be null...
     // next: &User,
-    //next: Option<User>,
+    // next: Option<User>,
     // neighbors: Vec<&User>,
 }
 
@@ -32,6 +32,14 @@ impl User {
             login_count: rhs.login_count,
         }
     }
+
+    fn name(&self) -> &str {
+        &self.name
+    }
+
+    fn set_name(&mut self, newname: String) {
+        self.name = newname;
+    }
 }
 
 fn main() {
@@ -50,31 +58,37 @@ fn main() {
         email: String::from("foo@bar.com"),
         ..thomas
     };
-    println!("Hello, world!\n{:?}", thomas2); // WOW - it is really moved!
 
-    let thomas3 = User::create_copy(&thomas2);
+    println!("Hello, world!\n{:?} {}", thomas.email, thomas.login_count); // WOW - it is really moved!
+
+    println!("Hello, world!\n{:?}", thomas2);
+
+    let mut thomas3 = User::create_copy(&thomas2);
 
     println!("Hello, world!\n{:?}\n{:?}", thomas2, thomas3);
+
+    thomas3.set_name(String::from("Foo"));
+    println!("{}", thomas3.name());
 }
 
 //------------------------------------------------------------------------------
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
 
-struct Circle {
-    radius: u32,
-}
+// struct Circle {
+//     radius: u32,
+// }
 
-impl Rectangle {
-    fn area(r: &Self) -> u32 {
-        r.width * r.height
-    }
-}
+// impl Rectangle {
+//     fn area(r: &Self) -> u32 {
+//         r.width * r.height
+//     }
+// }
 
-impl Circle {
-    fn area(c: &Self) -> f32 {
-        (c.radius as f32) * 2f32 * PI
-    }
-}
+// impl Circle {
+//     fn area(c: &Self) -> f32 {
+//         (c.radius as f32) * 2f32 * PI
+//     }
+// }
